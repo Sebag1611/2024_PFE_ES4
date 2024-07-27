fc93108d2377f6b43460f1224a0b18af2532c9aa
sebastian <sebastian.guerra17@inacapmail.cl>
2024-07-26 20:04:21 -0400

    añadido formularioFragment y modificado App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+/* core */
 import { useState } from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+    Container,
+    Row
+} from 'reactstrap'
 function App() {
+    const [usuarios, setUsuarios] = useState([])
+    const [nuevoUsuarioId, setNuevoUsuarioId] = useState('')
 
 
   return (
     <>
+      <Container>
+          <Row>
 
+          </Row>
+      </Container>
     </>
   )
 }
---
+++ src/fragments/FormularioFragment.jsx
@@ -0,0 +1,14 @@
+/* core */
+import { useState } from "react";
+import {} from 'reactstrap'
+
+/* component:view */
+function FormularioFragment() {
+    return (
+        <>
+        </>
+    )
+}
+
+/*Exportación*/
+export default FormularioFragment
